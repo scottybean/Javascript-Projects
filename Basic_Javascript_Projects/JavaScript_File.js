5599ce9a6e00306bd9eb82104d2fc846d79867d6
scottybean <60233939+scottybean@users.noreply.github.com>
2020-02-05 15:55:54 -0500

    Add files via upload

---
+++ Basic_Javascript_Projects/JavaScript_File.js
@@ -0,0 +1,12 @@
+var x = 'Hello, World!'
+window.alert(x);
+
+var a = "Barney said to Mickey, \"Hi!\""
++ " Then Mickey said to Barney, \"What\s up Doc?\" Which doesn\'t make much sense";
+document.write(a); 
+
+var Seahawks = "Best Football team", Sanfrancisco = "Worst football team", Raiders = "Neutral football team";
+var Seahawks = Seahawks.fontcolor("blue");
+document.write(Seahawks);
+
+document.write('3 + 3');
