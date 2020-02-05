ed6b116a7cf87988f7cc0beac1de682636cf6237
scottybean <60233939+scottybean@users.noreply.github.com>
2020-02-05 17:07:00 -0500

    Add files via upload

---
+++ Basic_Javascript_Projects/Basic_Javascript_2.js
@@ -0,0 +1,11 @@
+function Scotty(){
+    var x = "Heck yeah he is!"
+    var y = x.fontcolor("blue");
+document.getElementById("demo").innerHTML= y;
+}
+
+function table(){
+    var sentence = "I am the best pingpong player";
+    sentence += " in the world!";
+    document.getElementById("pingpong").innerHTML = sentence;
+}
